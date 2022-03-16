62050bc73382f3494bca02a0b9b5a009123d819b
suky <1724271@donga.ac.kr>
2022-03-16 22:31:56 +0900

    # 식당 정보 state에 저장

--- src/KakaoMap/KakaoMap.tsx
+++ src/KakaoMap/KakaoMap.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import styles from './KakaoMap.module.scss'
-// import { restaurantsInfo } from '../api/restuarants'
-
-
+import { restaurantsInfo } from '../api/restuarants'
+import { async } from '@firebase/util';
 
 declare global {
     interface Window {
@@ -15,10 +14,9 @@ type positionType = {
     lon: number
 }
 
-
 function KakaoMap() {
     const [position, setPosition] = useState({ lat: 0, lon: 0 })
-
+    const [markerData, setMarkerData] = useState([{title:'', latlng:''}])
 
     function createMap(position: positionType) {
 
@@ -30,16 +28,51 @@ function KakaoMap() {
         let map = new window.kakao.maps.Map(container, options)
 
         // * 지도에 교통정보 추가
-        map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.TRAFFIC);
+        // map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.TRAFFIC);
+    
+        // restaurant 마커 정보 추가
+        let positions = markerData;
+        // let positions = [
+        //     {
+        //         title: '카카오', 
+        //         latlng: new window.kakao.maps.LatLng(33.450705, 126.570677)
+        //     },
+        //     {
+        //         title: '생태연못', 
+        //         latlng: new window.kakao.maps.LatLng(33.450936, 126.569477)
+        //     },
+        // ];  
+        
+        
+        // 마커 이미지의 이미지 주소입니다
+        var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+            
+        for (var i = 0; i < positions.length; i ++) {
+            
+            // 마커 이미지의 이미지 크기 입니다
+            var imageSize = new window.kakao.maps.Size(24, 35); 
+            
+            // 마커 이미지를 생성합니다    
+            var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize); 
+            
+            // 마커를 생성합니다
+            var marker = new window.kakao.maps.Marker({
+                map: map, // 마커를 표시할 지도
+                position: positions[i].latlng, // 마커를 표시할 위치
+                title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                image : markerImage // 마커 이미지 
+            });
+        }
 
-        const markerPosition = new window.kakao.maps.LatLng(position.lat, position.lon);
 
-        var marker = new window.kakao.maps.Marker({
-            position: markerPosition
-        });
+        // const markerPosition = new window.kakao.maps.LatLng(position.lat, position.lon);
+
+        // var marker = new window.kakao.maps.Marker({
+        //     position: markerPosition
+        // });
 
         // 마커가 지도 위에 표시되도록 설정합니다
-        marker.setMap(map);
+        // marker.setMap(map);
 
     }
 
@@ -54,15 +87,30 @@ function KakaoMap() {
     }
 
     // ! 식당 예시 데이터 받아오는 함수
-    // const getRestaurantsInfo = async () => {
-    //     console.log(await restaurantsInfo());
-    // }
+    const getRestaurantsInfo = async () => {
+        console.log(await restaurantsInfo());
+        (await restaurantsInfo()).map(a => {
+            a.map((t:any) => {
+                console.log(t)
+                const data = {
+                    title:t.name,
+                    latlng:new window.kakao.maps.LatLng(t.location._lat, t.location._long)                    
+                }
+
+                setMarkerData(markerData => [...markerData, data]);
+                console.log(markerData)
+                
+            })  
+        });
+
+
+    }
 
 
     useEffect(() => {
+        getRestaurantsInfo();
         // 초기 위치 설정
         getCurrentPosition();
-        // getRestaurantsInfo();
     }, [])
 
     useEffect(() => {
@@ -72,6 +120,7 @@ function KakaoMap() {
 
     return (
         <>
+          
             <div id="map" className={styles['map']}>
                 <button className={styles['location']} onClick={getCurrentPosition}></button>
             </div>
