6a56471eb2e3a8a26834a22e1493602185b73487
suky <1724271@donga.ac.kr>
2022-12-18 23:59:06 +0900

    # KakaoMap map 간단한 수정

--- src/KakaoMap/KakaoMap.tsx
+++ src/KakaoMap/KakaoMap.tsx
@@ -38,11 +38,14 @@ function KakaoMap(props: any) {
     const [markerData, setMarkerData] = useState<markerType[]>([])
     const [addressInfo, setAddressInfo] = useState<addressType[]>([])
     const [address, setAddress] = useState<any[]>([])
-
+    const [currentPosition, setCurrentPosition] = useState({ lat: 0, lon: 0 })
+    
     // console.log(props.items);
     let items = props.items;
     let comment = props.comment;
     // console.log(comment)
+    let map: any;
+    let ce: any;
 
     function createMap(position: positionType) {
         let container = document.getElementById('map')
@@ -50,61 +53,25 @@ function KakaoMap(props: any) {
             center: new window.kakao.maps.LatLng(position.lat, position.lon),
             level: 12
         }
-        let map = new window.kakao.maps.Map(container, options)
+        map = new window.kakao.maps.Map(container, options)
+
+        getCommentInfo(map)
+
+        // console.log(markerData);
+    }
+
+    function createMap2() {
+        let container = document.getElementById('map')
+        let options: object = {
+            center: new window.kakao.maps.LatLng(map.getCenter().getLat(), map.getCenter().getLng()),
+            level: 12
+        }
+        map = new window.kakao.maps.Map(container, options)
 
         getCommentInfo(map)
         // console.log(markerData);
     }
 
-    function setMarker(map: object) {
-
-        // 텍스트 주소 -> 위치 주소로 변경
-        //  getCommentInfo();
-
-        // restaurant 마커 정보 추가
-        let positions = addressInfo
-        console.log(positions)
-
-        positions.map(data => {
-            // 마커 이미지의 이미지 주소
-            let imageSrc = Icon;
-
-            // 마커 이미지의 이미지 크기
-            let imageSize = new window.kakao.maps.Size(22, 22);
-
-            // 마커 이미지를 생성합니다    
-            let markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize,)
-
-            // { offset: new window.kakao.maps.Point(24, 24) }// 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.);) 
-            // 마커를 생성합니다
-
-
-            let marker = new window.kakao.maps.Marker({
-                map: map,
-                position: data.latlng,
-                title: data.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                image: markerImage // 마커 이미지 
-            });
-
-            // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능
-            let content = `<div id="customoverlay" class=${styles['customoverlay']} >` +
-                `  <a href="#" target="_blank">` +
-                `    <span class=${styles['title']}>${data.title}</span>` +
-                '  </a>' +
-                '</div>';
-
-            // 커스텀 오버레이가 표시될 위치
-            let position = new window.kakao.maps.LatLng(data.latlng);
-
-            // 커스텀 오버레이를 생성
-            let customOverlay = new window.kakao.maps.CustomOverlay({
-                map: map,
-                position: data.latlng,
-                content: content,
-                yAnchor: 1,
-            });
-        })
-    }
 
     function getCurrentPosition() {
         if (navigator.geolocation) {
@@ -215,8 +182,9 @@ function KakaoMap(props: any) {
     }, [])
 
     useEffect(() => {
+        // 지도 렌더링
         createMap(position)
-    }, [position, comment])
+    }, [position,comment])
 
     // useEffect(() => {
     //     createMap(position)
@@ -225,6 +193,12 @@ function KakaoMap(props: any) {
     // useEffect(() => {
     //     getCommentInfo(map);
     // }, [comment])
+  
+    useEffect(() => {
+        // 지도 렌더링
+        createMap2()
+    }, [comment])
+
 
     return (
         <>
